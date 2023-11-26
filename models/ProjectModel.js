50fb78932dffec3c2d48d17c6221523ada126ec0
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2023-11-26 17:28:32 +0530

    adding project list

--- models/ProjectModel.js
+++ models/ProjectModel.js
@@ -1,9 +1,17 @@
 const mongoose=require("mongoose");
+const AllProjectlist=new mongoose.Schema({
+    "title":String,
+    "description":String,
+    "status":Boolean,
+    "date":String,
+});
 const Projectschema=mongoose.Schema({
 Projectname:String,
 Projectdescription:String,
 Episodes:Number,
-email:String
+email:String,
+Projectlist:[AllProjectlist]
 });
+
 const ProjectfinalModel=mongoose.model("projectmodel",Projectschema);
 module.exports={ProjectfinalModel};
